9a2c406495685da2d87e7778bcfa5b82cb126bc5
Olivia Artz <liv@oliviaartz.com>
2021-11-22 20:13:23 -0500

    Starting to layout the case studies

--- wp-content/themes/convocation/single.php
+++ wp-content/themes/convocation/single.php
@@ -4,7 +4,7 @@
 
     <section class="use-case">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h2><?php the_title() ;?></h2>
+            <!-- <h2><?php the_title() ;?></h2> -->
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
     </section>
