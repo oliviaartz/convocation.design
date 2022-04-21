52ea42a2715ae9610534622d878e227d3109c386
Olivia Artz <liv@oliviaartz.com>
2022-04-21 19:56:17 -0400

    working on case studies

--- wp-content/themes/convocation/single-case_study.php
+++ wp-content/themes/convocation/single-case_study.php
@@ -4,8 +4,20 @@
 
     <section class="use-case">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_title(); ?>
-            <?php get_project_date_range(); ?>
+            <div class="use-case-info">
+                <h2 class="use-case-title"><?php the_title(); ?></h2>
+                <div class="use-case-date"><?php get_project_date_range(); ?></div>
+                <?php   $areas = get_field('areas_of_focus'); ?>
+                    <div class="use-case-areas">
+                        <?php if( $areas ):
+                            foreach( $areas as $area ): 
+                                $area_term = get_term_by('term_taxonomy_id', $area, 'areas_of_focus');
+                                $area_name = get_term( $area_term )->name ?>
+                                <a href="#"><?php echo $area_name; ?></a>
+                            <?php endforeach; ?>
+                        <?php endif; ?> 
+                    </div>
+            </div>
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
     </section>
